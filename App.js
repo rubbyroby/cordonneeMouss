26298083219223ed4047b886cfe54f6dd3c09f40
rubbyroby <devdigapp@gmail.com>
2023-09-01 16:09:41 +0100

    Add MIT licence

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import '../src/index.css'
+import Coordonnee from "./component/Coordonnee";
+export default function App(){
+return (
+<div className="App">
+<Coordonnee/>
+</div>
+)
+}
